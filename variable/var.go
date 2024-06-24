724cd23f15aa103e5d560f62dab5bd2ee29815d6
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-24 15:04:48 +0700

    [WIP] Declare Variables

---
+++ variable/var.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var firstName string = "Mechiuw"
+
+	// lastName := "Diamonda"
+	// age := 10
+	// fmt.Println(firstName, lastName)
+	// fmt.Printf("my age is : %d \n", age)
+
+	// var (
+	// 	bootCampName, bootCampAddress = "Enigma Camp", "Malang"
+	// )
+	// fmt.Printf("name : %s || address : %s", bootCampName, bootCampAddress)
+	variables()
+}
+
+func variables() {
+	username := "mechiuw"
+	gmail := "matthewdpk@gmail.com"
+	fmt.Printf("username : %s\n", username)
+	fmt.Printf("gmail : %s", gmail)
+}
