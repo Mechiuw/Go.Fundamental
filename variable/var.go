8e401aac3460e7c8f89f9871baf3c509e211036c
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-24 15:12:15 +0700

    [WIP] Declare Multiple Variables

--- variable/var.go
+++ variable/var.go
@@ -14,12 +14,35 @@ func main() {
 	// 	bootCampName, bootCampAddress = "Enigma Camp", "Malang"
 	// )
 	// fmt.Printf("name : %s || address : %s", bootCampName, bootCampAddress)
+
 	variables()
+	multiple()
 }
 
 func variables() {
 	username := "mechiuw"
 	gmail := "matthewdpk@gmail.com"
 	fmt.Printf("username : %s\n", username)
-	fmt.Printf("gmail : %s", gmail)
+	fmt.Printf("gmail : %s\n", gmail)
+}
+
+func multiple() {
+	var a, b, c, d string = "hey", "hi", "hello", "heyow"
+	fmt.Println(a, b, c, d)
+
+	var w, x, y, z = 10, 54, 12, 38
+	fmt.Println(w, x, y, z)
+
+	var number, text = 10, "this is ten"
+	fmt.Println(number, text)
+
+	one, two, three := 1, true, "woah"
+	fmt.Println(one, two, three)
+
+	var (
+		x1        = 10
+		x2 int    = 20
+		x3 string = "example"
+	)
+	fmt.Println(x1, x2, x3)
 }
