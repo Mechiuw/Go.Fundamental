858ea7fa40a51f9d28e92ae9459345bb2f3ce7f9
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-24 19:15:16 +0700

    [WIP] go-map

---
+++ map/map.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var a = map[string]string{"brand": "ford", "model": "mustang", "year": "1964"}
+	// fmt.Println(a)
+
+	var b = make(map[int]string)
+	b[1] = "teacher"
+	b[2] = "techinician"
+	b[3] = "engineer"
+	b[4] = "judge"
+	b[5] = "entrepeneur"
+
+	for i := 1; i < len(b); i++ {
+		fmt.Println(i, b[i])
+	}
+}
